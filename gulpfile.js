f37cd206021425c5e821eebc7d8f65ab8fdd05db
Hasan Ozdemir <office@nodify.at>
2016-09-22 00:29:15 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var path = {
         './src/bubble-tree.js',
         './src/bubble-factory.js',
         './src/lib/jquery.browser.min.js',
-        './src/lib/raphael.js',
+        // './src/lib/raphael.js',
         './src/lib/Tween.js',
         './src/lib/vis4.js',
         './src/lib/bubbletree.js'
